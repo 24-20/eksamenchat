8ca70aeebcae439ea83ab20d8e11b6ff0aa17e27
Alex <aleksander.a.sivertsen@gmail.com~>
2025-06-11 20:20:22 +0200

    stuff

--- frontend/components/chate_area/message.tsx
+++ frontend/components/chate_area/message.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
@@ -6,6 +8,7 @@ import rehypeRaw from 'rehype-raw';
 import 'katex/dist/katex.min.css';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { CheckIcon, Copy } from 'lucide-react';
 
 interface MessageProps {
   content: string;
@@ -20,21 +23,37 @@ interface CodeProps {
 }
 
 const Message: React.FC<MessageProps> = ({ content, isUser }) => {
+  const [copyclipboard, setcopyclipboard] = useState<boolean>(false)
   const components = {
     code({ node, inline, className, children, ...props }: CodeProps) {
       const match = /language-(\w+)/.exec(className || '');
       const lang = match?.[1] || 'text';
       const code = String(children).replace(/\n$/, '');
-
       return !inline ? (
-        <div className="relative my-4 rounded-lg bg-neutral-900 overflow-hidden">
-          <div className="flex justify-between items-center px-4 py-2 bg-neutral-800 border-b border-neutral-700 text-sm text-neutral-300">
-            <span>{lang}</span>
+        <div className="relative mb-4 rounded-lg bg-neutral-900 overflow-hidden">
+          <div className="flex justify-between items-center px-4 py-2 bg-secondary border-b border-neutral-700 text-sm text-foreground">
+            <span className=' text-xs text-foreground/80'>{lang}</span>
             <button
-              onClick={() => navigator.clipboard.writeText(code)}
-              className="text-xs px-2 py-1 bg-neutral-700 hover:bg-neutral-600 rounded"
+              onClick={() => {
+                navigator.clipboard.writeText(code)
+                setcopyclipboard(true)
+
+              }}
+              className="text-xs text-foreground/80 px-2 py-1 hover:bg-foreground/20 rounded flex items-center justify-center gap-1 "
             >
-              Copy
+              {
+                copyclipboard ?
+                  <CheckIcon className='text-xs w-3.5 h-3.5' />
+                  :
+
+                  <Copy className='text-xs w-3.5 h-3.5' />
+              }
+              {
+                copyclipboard ?
+                  <>Kopiert</>
+                  :
+                  <>Kopier</>
+              }
             </button>
           </div>
           <SyntaxHighlighter
@@ -54,6 +73,14 @@ const Message: React.FC<MessageProps> = ({ content, isUser }) => {
     },
   };
 
+
+  useEffect(() => {
+    if (copyclipboard) {
+      setTimeout(() => {
+        setcopyclipboard(false)
+      }, 2000)
+    }
+  }, [copyclipboard])
   return (
     <div
       className={`flex pb-12 h-full w-full ${isUser ? 'items-end justify-end' : 'items-start'
@@ -61,8 +88,8 @@ const Message: React.FC<MessageProps> = ({ content, isUser }) => {
     >
       <div
         className={`p-4 rounded-lg ${isUser
-            ? 'bg-foreground/5 self-end max-w-[350px] w-fit'
-            : 'self-start max-w-2xl w-full'
+          ? 'bg-foreground/5 self-end max-w-[350px] w-fit'
+          : 'self-start max-w-2xl w-full'
           }`}
       >
         <ReactMarkdown
